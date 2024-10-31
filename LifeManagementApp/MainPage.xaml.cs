8ec2d6543053fa67b6fad179b705e7cf2a435287
ZlataShabalina <atalz2004@gmail.com>
2024-10-31 11:16:38 +0200

    Created the first version of the application, check weather feature.

---
+++ LifeManagementApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace LifeManagementApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void WeatherClick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Weather());
+        }
+    }
+
+}
